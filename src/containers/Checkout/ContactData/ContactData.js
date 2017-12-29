5d46293f1ead65dc780eb00a9492593f85d1c0c0
Alex Guevara <a.guevara23@gmail.com>
2017-12-28 19:05:26 -0500

    add 2 way binding to order form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -68,10 +68,14 @@ class ContactData extends Component {
     event.preventDefault();
     console.log(this.props.ingredients)
     this.setState({loading: true});
-
+    const formData = {};
+    for ( let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     }
 
     axios.post('/orders.json', order)
@@ -84,6 +88,18 @@ class ContactData extends Component {
       })
   }
 
+  inputChangedHandler = (event, inputIdentifier ) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    }
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
+  }
+
   render() {
     let formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -94,15 +110,16 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value} />
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
         ))}
-        <Button clicked={this.orderHandler} btnType='Success'>ORDER</Button>
+        <Button btnType='Success'>ORDER</Button>
       </form>
     )
     if (this.state.loading) {
